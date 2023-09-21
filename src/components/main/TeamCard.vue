95b0cf7c209487384561ce76946fc737690f1621
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-21 10:39:59 +0200

    added icons to TeamCard

--- src/components/main/TeamCard.vue
+++ src/components/main/TeamCard.vue
@@ -16,10 +16,36 @@ export default {
       voluptatum quo fuga fugit nulla nesciunt, iste eos tempore impedit
       voluptates debitis modi atque facere, corrupti earum, iusto laborum? A.
     </p>
-    <span> instagram facebook twitter </span>
+    <font-awesome-icon icon="fa-brands fa-linkedin-in" /><font-awesome-icon
+      icon="fa-brands fa-facebook-f"
+    /><font-awesome-icon class="icon" icon="fa-brands fa-twitter" />
   </div>
-  <div class="button prev">arrow</div>
-  <div class="button next">arrow</div>
+  <div class="button prev">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      style="height: 32px"
+      fill="#1e1c1c"
+    >
+      <path
+        d="M4 15a1 1 0 001 1h19.586l-4.292 4.292a1 1 0 001.414 1.414l6-6a.99.99 0 00.292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 00-.216-.324l-6-6a1 1 0 00-1.414 1.414L24.586 14H5a1 1 0 00-1 1z"
+      />
+    </svg>
+  </div>
+  <div class="button next">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      style="height: 32px"
+      fill="#1e1c1c"
+    >
+      <path
+        d="M4 15a1 1 0 001 1h19.586l-4.292 4.292a1 1 0 001.414 1.414l6-6a.99.99 0 00.292-.702V15c0-.13-.026-.26-.078-.382a.99.99 0 00-.216-.324l-6-6a1 1 0 00-1.414 1.414L24.586 14H5a1 1 0 00-1 1z"
+      />
+    </svg>
+  </div>
 </template>
 
 <style lang="scss" scoped>
